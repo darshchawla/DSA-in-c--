367f43917c017271ec1275aa06a3a2e69d56e18b
Darsh Chawla <darshdchawla@gmail.com>
2026-07-29 23:03:04 +0530

    pass by reference

---
+++ Basics/passByReference.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// pass by reference
+void print(int &a)
+{
+    cout << a << endl;
+    a += 5;
+    cout << a << endl;
+    a += 5;
+    cout << a << endl;
+}
+int main()
+{
+    int a = 10;
+
+    print(a);
+
+    cout << a << endl;
+}
