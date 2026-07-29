65dea90a5f4ce2a3ac37846cc1c7f66c10fd43b7
Darsh Chawla <darshdchawla@gmail.com>
2026-07-29 18:18:17 +0530

    first commit

---
+++ Basics/allDataTypes.cpp
@@ -0,0 +1,33 @@
+include <iostream>
+using namespace std;
+
+int main()
+{
+    // int
+    int x = 7;
+    cout << x << endl;
+
+    // long
+    long y = 15;
+    cout << y << endl;
+
+    // long long
+    long long z = 150000000;
+    cout << z << endl;
+
+    // float, double
+    float a = 7.5;
+    float b = 7;
+    double c = 9;
+    cout << a << ", " << b << ", " << c << endl;
+
+    // char
+    char ch = 'a';
+    cout << ch << endl;
+
+    // string
+    string str;
+    cout << "Enter a string : ";
+    cin >> str;
+    cout << str << endl;
+}
