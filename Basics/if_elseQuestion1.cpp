3ec111cff9dbefc17a499530d85fe8c128616b48
Darsh Chawla <darshdchawla@gmail.com>
2026-07-29 18:49:46 +0530

    if else question 1

---
+++ Basics/if_elseQuestion1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Enter an age (in years) : ";
+    cin >> age;
+
+    cout << endl;
+
+    if (age >= 18)
+    {
+        cout << "You are an adult." << endl;
+    }
+
+    else
+    {
+        cout << "You are not an adult." << endl;
+    }
+}
