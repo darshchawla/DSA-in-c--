728d293dd950f14c003fb2c137ad14eae4efa253
Darsh Chawla <darshdchawla@gmail.com>
2026-07-29 20:52:19 +0530

    arrays

---
+++ Basics/arrays.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    cout << "Enter elements of the array : ";
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+    cout << endl;
+
+    cout << "The elements are : " << arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3] << " " << arr[4] << " " << endl;
+
+    return 0;
+}
