eaa82d125a89a4a430131643042efcbf236e5a72
Darsh Chawla <darshdchawla@gmail.com>
2026-07-30 18:03:46 +0530

    pattern 16

---
+++ Patterns/pattern16.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << (char)(i + 64) << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter number of lines : ";
+    cin >> n;
+
+    print(n);
+}
