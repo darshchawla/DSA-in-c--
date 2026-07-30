75df828f88dbf7908b45f9795766a21a18975e24
Darsh Chawla <darshdchawla@gmail.com>
2026-07-30 14:01:59 +0530

    star triangle

---
+++ Patterns/starTriangle.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int l)
+{
+    for (int i = 1; i <= l; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int l;
+    cout << "Enter number of lines : ";
+    cin >> l;
+
+    cout << endl;
+
+    print(l);
+}
