cfe2609f7f556597fc2ab5ea3b665e51c3dad6c1
Darsh Chawla <darshdchawla@gmail.com>
2026-07-31 17:29:31 +0530

    factorial of all numbers from 1 to 'n'

---
+++ Pseudocode/factorialOfAllFrom1to'n'.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+
+    cout << endl;
+
+    int product = 1;
+    int count = 1;
+    while (count <= n)
+    {
+        product = product * count;
+        count += 1;
+        cout << "The factorial of " << n << " is : " << product << endl;
+        cout << endl;
+    }
+}
