156becb36c49a7db7ae39eeff2692ddc8d246719
Darsh Chawla <darshdchawla@gmail.com>
2026-07-31 14:16:23 +0530

    area of square

---
+++ Pseudocode/areaOfSquare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a;
+    cout << "Enter side of a square : ";
+    cin >> a;
+
+    cout << endl;
+
+    float area = a * a;
+    cout << "The area of the square is : " << area << endl;
+}
