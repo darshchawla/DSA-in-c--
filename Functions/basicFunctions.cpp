b549cdd064f05fee92516267ae259546f8333e59
Darsh Chawla <darshdchawla@gmail.com>
2026-08-01 15:38:57 +0530

    basics of functions

---
+++ Functions/basicFunctions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Function definition
+void printHello()
+{
+    cout << "Hello World." << endl;
+}
+
+int main()
+{
+    // Function call / invoke
+    printHello();
+    printHello();
+
+    return 0;
+}
