7659d87beaa93caf195ba3ba7204f0eb7b67ce50
Darsh Chawla <darshdchawla@gmail.com>
2026-08-01 22:04:35 +0530

    inverted number triangle

---
+++ Patterns/invertedTriangle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of rows : ";
+    cin >> n;
+
+    cout << endl;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i - 1; j++)
+        {
+            cout << "  ";
+        }
+        for (int k = 1; k <= n - i + 1; k++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
