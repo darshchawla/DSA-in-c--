a312854558b097e95d088a25630aed412dc4a232
Darsh Chawla <darshdchawla@gmail.com>
2026-08-04 12:37:14 +0530

    checking whether a year is leap year or not

---
+++ Basics/leapYearOrNot.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout << "Enter a year : ";
+    cin >> year;
+
+    cout << endl;
+
+    if (year % 4 == 0)
+    {
+        cout << "The given year is a leap year." << endl;
+    }
+    else
+    {
+        cout << "The given year is not a leap year." << endl;
+    }
+
+    return 0;
+}
