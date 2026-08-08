c79357767e74fdac449d02f67cc46cebca56c2f8
Darsh Chawla <darshdchawla@gmail.com>
2026-08-08 11:43:06 +0530

    finding second largest element in the array

---
+++ Arrays/secondLargest.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter the size of array: ";
+    cin >> size;
+
+    cout << endl;
+
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        int a = i + 1;
+        cout << "Enter element number " << a << " : ";
+        cin >> arr[i];
+        cout << endl;
+    }
+
+    int largest = arr[0];
+
+    int secondLargest = arr[0];
+
+    // Find largest
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    cout << "The largest element in the array is : " << largest << endl;
+
+    cout << endl;
+
+    // Find second largest
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > secondLargest && arr[i] < largest)
+        {
+            secondLargest = arr[i];
+        }
+    }
+
+    cout << "The second largest element in the array is : " << secondLargest << endl;
+
+    return 0;
+}
