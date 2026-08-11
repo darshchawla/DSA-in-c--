e53e09a226bffee0b13133f44588ab1223a24f11
Darsh Chawla <darshdchawla@gmail.com>
2026-08-11 10:21:18 +0530

    pair sum

---
+++ Arrays/Majority Element and Moore's Algorithm/pairSum.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter the size of the array : ";
+    cin >> size;
+
+    cout << endl;
+
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        int a = i + 1;
+        cout << "Enter element number " << a << " : ";
+        cin >> arr[i];
+        cout << endl;
+    }
+
+    cout << "The array is : ";
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+    cout << endl;
+
+    int target;
+    cout << "Enter a target : ";
+    cin >> target;
+
+    cout << endl;
+
+    bool found = false;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] + arr[j] == target)
+            {
+                if (found != false)
+                {
+                    cout << "The pair with targeted sum " << target << " is : ";
+                }
+                cout << "(" << arr[i] << " , " << arr[j] << ")" << endl;
+
+                found = true;
+            }
+        }
+    }
+
+    if (found == false)
+    {
+        cout << "No pair found for the target " << target << "." << endl;
+    }
+
+    return 0;
+}
