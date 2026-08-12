1094c62645e3e6ee49c51a10010b4c0c7c5ce7b3
Darsh Chawla <darshdchawla@gmail.com>
2026-08-12 19:34:33 +0530

    reverse number

--- Basics/reverseNumber.cpp
+++ Basics/reverseNumber.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <climits>
 using namespace std;
+
+int reverse(int n)
+{
+    int lastDigit;
+    int reverse = 0;
+    while (n > 0)
+    {
+        lastDigit = n % 10;
+        if ((reverse > INT_MAX / 10) || (reverse < INT_MIN / 10))
+        {
+            return 0;
+        }
+        reverse = (reverse * 10) + lastDigit;
+        n = n / 10;
+    }
+
+    return reverse;
+}
 
 int main()
 {
@@ -9,15 +28,9 @@ int main()
 
     cout << endl;
 
-    int lastDigit;
-    int reverse = 0;
-    while (n > 0)
-    {
-        lastDigit = n % 10;
-        reverse = (reverse * 10) + lastDigit;
-        n = n / 10;
-    }
-    cout << "The reverse of the given number is : " << reverse << endl;
+    int r = reverse(n);
+
+    cout << "The reverse of the given number is : " << r << endl;
 
     return 0;
 }
