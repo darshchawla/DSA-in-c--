897b27478e173e29517a8802f779af5d97728bc4
Darsh Chawla <darshdchawla@gmail.com>
2026-08-13 11:21:23 +0530

    nPr function

---
+++ Functions/nPrFunction.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    int factorial = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        factorial = factorial * i;
+    }
+
+    return factorial;
+}
+
+int nPr(int n, int r)
+{
+    int numerator = factorial(n);
+
+    int denominator = factorial(n - r);
+
+    int nPr = numerator / denominator;
+
+    return nPr;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+
+    cout << endl;
+
+    int r;
+    cout << "Enter r : ";
+    cin >> r;
+
+    cout << endl;
+
+    factorial(n);
+
+    int answer = nPr(n, r);
+
+    cout << "The answer is : " << answer << endl;
+
+    return 0;
+}
