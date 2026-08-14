0fc2b8a7a3d9ea57b5db5b5ffbafcbe9f070597e
Darsh Chawla <darshdchawla@gmail.com>
2026-08-14 13:51:52 +0530

    find duplicate element in the array (brute force approach)

---
+++ Arrays/Arrays Leetcode Problems/findDuplicateEasy.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int findDuplicate(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                return arr[i];
+            }
+        }
+    }
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of the array : ";
+    cin >> size;
+
+    cout << endl;
+
+    int arr[size];
+
+    // Number from 1 to size - 1
+
+    for (int i = 0; i < size; i++)
+    {
+        int a = i + 1;
+        ;
+        cout << "Enter element number " << a << " : ";
+        cin >> arr[i];
+        cout << endl;
+    }
+
+    cout << "The array is : ";
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+    cout << endl;
+
+    int answer = findDuplicate(arr, size);
+
+    cout << "The duplicate element in the array is : " << answer << endl;
+
+    return 0;
+}
