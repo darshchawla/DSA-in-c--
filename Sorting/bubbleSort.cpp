bbb3da275f44fbb7f9a93ae193801d92d61b6efc
Darsh Chawla <darshdchawla@gmail.com>
2026-08-17 22:48:06 +0530

    bubble sort

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -5,6 +5,8 @@ void bubble_sort(int arr[], int size)
 {
     for (int i = 1; i < size; i++)
     {
+        bool swapped = false;
+
         for (int j = 0; j < size - i; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -12,7 +14,13 @@ void bubble_sort(int arr[], int size)
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+
+                swapped = true;
             }
+        }
+        if (swapped == false)
+        {
+            break;
         }
     }
 }
