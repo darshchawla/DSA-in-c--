df2cbdfc2282f98b3130692503b914b994ef2b00
Darsh Chawla <darshdchawla@gmail.com>
2026-08-20 10:22:26 +0530

    replace spaces to @40 in string

---
+++ Strings/replaceSpaces.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+string replaceSpaces(string s)
+{
+    string temp = "";
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else
+        {
+            temp.push_back(s[i]);
+        }
+    }
+
+    return temp;
+}
+
+int main()
+{
+    string s;
+    cout << "Enter a string : ";
+    getline(cin, s);
+
+    cout << endl;
+
+    cout << "The string is : ";
+
+    cout << s << endl;
+
+    cout << endl;
+
+    s = replaceSpaces(s);
+
+    cout << "The string after replacing spaces to @40 is : " << s << endl;
+
+    return 0;
+}
