3b2a9966e015e59ebab66a0207e7d0bf82b49851
Darsh Chawla <darshdchawla@gmail.com>
2026-08-21 18:55:25 +0530

    count primes (Sieve of Eratosthenes)

---
+++ Maths for DSA/countPrimes.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countPrime(int n)
+{
+    int count = 0;
+    vector<bool> prime(n + 1, true);
+
+    prime[0] = prime[1] = false;
+
+    for (int i = 2; i < n; i++)
+    {
+        if (prime[i])
+        {
+            count++;
+
+            for (int j = 2 * i; j < n; j += i)
+            {
+                prime[j] = 0;
+            }
+        }
+    }
+
+    return count;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+
+    cout << endl;
+
+    int a = countPrime(n);
+
+    cout << "There are " << a << " prime numbers lees than " << n << "." << endl;
+
+    return 0;
+}
