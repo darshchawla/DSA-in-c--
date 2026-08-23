b70d443ddfecb0770a8ddf6af7dfd411f618b32c
Darsh Chawla <darshdchawla@gmail.com>
2026-08-23 11:21:37 +0530

    power (a,b)

---
+++ Recursion/power(a,b).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int power(int a, int b)
+{
+    if (b == 1)
+        return a;
+
+    int p = a * power(a, b - 1);
+    return p;
+}
+
+int main()
+{
+    int a;
+    cout << "Enter base : ";
+    cin >> a;
+
+    cout << endl;
+
+    int b;
+    cout << "Enter exponent : ";
+    cin >> b;
+
+    cout << endl;
+
+    int c = power(a, b);
+
+    cout << a << " raised to the power " << b << " is : " << c << endl;
+
+    return 0;
+}
