54dc8ae32dee31744d21fd44fb365928a745d69f
Darsh Chawla <darshdchawla@gmail.com>
2026-08-24 14:19:44 +0530

    check whether a string is palindrome or not

--- Recursion/stringisPalindromeOrNot.cpp
+++ Recursion/stringisPalindromeOrNot.cpp
@@ -1,26 +1,55 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-bool palindrome(string &str, int i)
+bool palindrome(string &str, int i, int j)
 {
-    if (i >= str.size() / 2)
+    // Base Case
+    if (i > j)
+    {
         return true;
-    if (str[i] != str[str.size() - i - 1])
+    }
+
+    if (str[i] != str[j])
+    {
         return false;
-    return palindrome(str, i + 1);
+    }
+    else
+    {
+        // Recursive Call
+        return palindrome(str, i + 1, j - 1);
+    }
 }
 
 int main()
 {
-    string str = "radar";
+    string str;
+    cout << "Enter a string : ";
+    getline(cin, str);
 
     cout << endl;
 
-    cout << palindrome(str, 0);
+    cout << "The string is : ";
+
+    cout << str << endl;
+
+    cout << endl;
+
+    int i = 0;
+    int j = str.length() - 1;
+
+    bool isPalindrome = palindrome(str, i, j);
 
     // true = 1 = string is a palindrome
     // false = 0 = string is not a palindrome
+
+    if (isPalindrome == 1)
+    {
+        cout << "The string is palindrome." << endl;
+    }
+    else
+    {
+        cout << "The string is not a palindrome." << endl;
+    }
 
     return 0;
 }
