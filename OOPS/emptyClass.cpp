9aa2541261226cd6c6da99e6c3a2bef3c0c0596f
Darsh Chawla <darshdchawla@gmail.com>
2026-08-26 12:25:54 +0530

    empty class

---
+++ OOPS/emptyClass.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Hero // Empty class -> No properties
+{
+};
+
+int main()
+{
+
+    // Creation of object
+    Hero h1;
+
+    cout << "Size : " << sizeof(h1) << endl;
+
+    return 0;
+}
