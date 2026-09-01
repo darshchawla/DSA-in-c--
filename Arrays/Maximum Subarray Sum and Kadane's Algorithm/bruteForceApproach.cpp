216f23c7524b4007ad3c5e1d75722041fa36e43c
Darsh Chawla <darshdchawla@gmail.com>
2026-09-01 22:29:37 +0530

    maximum subarray sum(Brute Force Approach)

---
+++ Arrays/Maximum Subarray Sum and Kadane's Algorithm/bruteForceApproach.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
+
+void printSubarrays(vector<int> &arr, int size)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i; j < size; j++)
+        {
+            cout << "{ ";
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k] << " ";
+            }
+            cout << "}   ";
+        }
+        cout << endl;
+    }
+}
+
+int maximumSubarraySum(vector<int> &arr, int size)
+{
+    int maxSum = INT_MIN;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i; j < size; j++)
+        {
+            int sum = 0;
+
+            for (int k = i; k <= j; k++)
+            {
+                sum += arr[k];
+            }
+            maxSum = max(maxSum, sum);
+        }
+    }
+
+    return maxSum;
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of the array : ";
+    cin >> size;
+
+    cout << endl;
+
+    vector<int> arr(size);
+
+    cout << "Enter all the elements of the array : ";
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << endl;
+
+    cout << "The array is : { ";
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i];
+
+        if (i != size - 1)
+        {
+            cout << ", ";
+        }
+    }
+
+    cout << " }";
+    cout << endl;
+
+    cout << "The subarrays are : " << endl;
+
+    printSubarrays(arr, size);
+
+    cout << endl;
+
+    int answer = maximumSubarraySum(arr, size);
+
+    cout << "The maximum subarray sum is : " << answer << endl;
+    ;
+
+    return 0;
+}
