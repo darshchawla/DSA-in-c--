8d33ab9a2591cf0674fe71da58139ac333e1ffe0
Darsh Chawla <darshdchawla@gmail.com>
2026-09-01 08:44:07 +0530

    majority element (Brute Force Approach)

--- Arrays/Majority Element and Moore's Algorithm/majorityElement.cpp
+++ Arrays/Majority Element and Moore's Algorithm/majorityElement.cpp
@@ -2,6 +2,27 @@
 #include <vector>
 using namespace std;
 
+int majorityElement(vector<int> &nums, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < size; j++)
+        {
+            if (nums[j] == nums[i])
+            {
+                count++;
+            }
+        }
+        if (count > (size / 2))
+        {
+            return nums[i];
+        }
+    }
+
+    return -1;
+}
+
 int main()
 {
     int size;
@@ -10,33 +31,36 @@ int main()
 
     cout << endl;
 
-    vector<int> number(size);
+    vector<int> nums(size);
 
     cout << "Enter all the elements of the vector : ";
 
     for (int i = 0; i < size; i++)
     {
-        cin >> number[i];
+        cin >> nums[i];
     }
 
     cout << endl;
 
-    for (int value : number)
+    cout << "The array is : { ";
+
+    for (int i = 0; i < size; i++)
     {
-        int count = 0;
-        for (int element : number)
+        cout << nums[i];
+
+        if (i != size - 1)
         {
-            if (element == value)
-            {
-                count++;
-            }
-        }
-        if (count > size / 2)
-        {
-            cout << "The majority element is : " << value << endl;
-            break;
+            cout << ", ";
         }
     }
+
+    cout << " }";
+    cout << endl;
+    cout << endl;
+
+    int answer = majorityElement(nums, size);
+
+    cout << "The majority element in the array is : " << answer << endl;
 
     return 0;
 }
